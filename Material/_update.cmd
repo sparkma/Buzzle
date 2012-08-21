4e15e56e0e5b31e2b3157436b58c9915489a42f8
david.win32 <umbrahan@gmail.com>
2012-08-21 18:34:07 +0800

    rest

---
+++ Material/_update.cmd
@@ -0,0 +1,13 @@
+copy .\bubbles\bubbles.plist ..\Resources\game\common\ /y
+copy .\bubbles\bubbles.png ..\Resources\game\common\ /y
+
+copy .\doodads\doodads_menu.plist ..\Resources\game\common\ /y
+copy .\doodads\doodads_menu.png ..\Resources\game\common\ /y
+
+copy .\props\props.plist ..\Resources\game\common\ /y
+copy .\props\props.png ..\Resources\game\common\ /y
+
+copy .\sparks\sparks.plist ..\Resources\game\common\ /y
+copy .\sparks\sparks.png ..\Resources\game\common\ /y
+
+pause
